ffcdecfe5ca767d7e069b19922abca9e649daa81
Piotr <piotr.krzysztof.wojcik@gmail.com>
2017-05-14 15:48:46 +0200

    JS buttons listeners added

--- js/game.js
+++ js/game.js
@@ -1,4 +1,15 @@
-//New game button listener
+//New game button find by DOM
 var newGameBtn = document.getElementById('js-newGameButton');
+
+//New game listener
 newGameBtn.addEventListener('click', newGame);
 
+//pickButton find by DOM
+var pickRock = document.getElementById("js-playerPick_rock"),
+	pickPaper = document.getElementById("js-playerPick_paper"),
+	pickScissors = document.getElementById("js-playerPick_scissors");
+
+//Listener for pickButtons with function playerPick with specified parameter
+pickRock.addEventListener('click', function() {playerPick('rock');});
+pickPaper.addEventListener('click', function() {playerPick('paper');});
+pickScissors.addEventListener('click', function() {playerPick('scissors');});
