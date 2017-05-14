d070e04e5c8bcde9159ec6dc7fb6a9acb65f3ed9
Piotr <piotr.krzysztof.wojcik@gmail.com>
2017-05-14 19:23:30 +0200

    Function setGameElements completed

--- js/game.js
+++ js/game.js
@@ -1,15 +1,50 @@
-//New game button find by DOM
-var newGameBtn = document.getElementById('js-newGameButton');
+//Variables which indicates particular parts of the game
+var newGameBtn = document.getElementById('js-newGameButton'),
+	newGameElem = document.getElementById('js-newGameElement'),
+	pickElem = document.getElementById('js-playerPickElement'),
+	resultsElem = document.getElementById('js-resultsTableElement');
+	console.log(newGameBtn);
 
 //New game listener
-newGameBtn.addEventListener('click', newGame);
+// newGameBtn.addEventListener('click', newGame);
 
 //pickButton find by DOM
 var pickRock = document.getElementById("js-playerPick_rock"),
 	pickPaper = document.getElementById("js-playerPick_paper"),
 	pickScissors = document.getElementById("js-playerPick_scissors");
 
-//Listener for pickButtons with function playerPick with specified parameter
+//Listener for pickButtons with function playerPick with specified parameter (rock, paper, scissor)
 pickRock.addEventListener('click', function() {playerPick('rock');});
 pickPaper.addEventListener('click', function() {playerPick('paper');});
 pickScissors.addEventListener('click', function() {playerPick('scissors');});
+
+//Initialize default values.
+var gameState = 'notStarted',  //gameState is for customize display game elements
+	player = {
+		name: '',
+		score: 0
+	},
+	computer = {
+		score: 0
+	};
+
+//Function for game state
+
+function setGameElements() {
+	switch(gameState) {
+		case 'started':
+			newGameElem.style.display = 'none';
+			pickElem.style.display = 'flex';
+			resultsElem.style.display = 'block';
+		break;
+		case 'ended':
+			newGameBtn.innerText = 'One more time';
+		case 'notStarted':
+		default:
+			newGameElem.style.display = 'flex';
+			pickElem.style.display = 'none';
+			resultsElem.style.display = 'none';
+	}
+}
+setGameElements();
+
